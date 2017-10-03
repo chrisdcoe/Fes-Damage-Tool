a29082c2ffe9ee5c5ef31a8adeae11e9cf29b775
Christopher Coe <christopherdcoe@gmail.com>
2017-10-03 16:56:36 -0400

    Created project, added index and app.js, initialize variables

---
+++ app.js
@@ -0,0 +1,25 @@
+//Player stats
+var playerStr = 0;
+var playerDef = 0;
+var playerMagic = 2;
+var playerInt = 2;
+var playerSpd = 2;
+
+//Weapon stats
+var weaponAttack = 0;
+var weaponMagic = 1.00;
+var weaponElement = "None";
+var weaponAttackNum = 1;
+
+//Skill stats
+var skillElement = "None";
+var skillIsMagic = false;
+var skillDamage = 0;
+var skillAttackNum = 1;
+
+//Monster stats
+var monsterAttack = 0;
+var monsterDef = 0;
+var monsterMagDef = 0;
+var monsterInt = 0;
+var monsterSpd = 1;
