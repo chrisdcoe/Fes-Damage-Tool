e79ff2323313b3ea4d70c8b54df644d62b34d07c
Christopher Coe <christopherdcoe@gmail.com>
2017-11-01 21:14:05 -0400

    Added a lot of basic framework for the user interface

--- app.js
+++ app.js
@@ -1,40 +1,14 @@
-//Player stats
-var playerStr = 5;
-var playerDef = 0;
-var playerMagic = 2;
-var playerInt = 2;
-var playerSpd = 2;
-
-//Weapon stats
-var weaponAttack = 4;
-var weaponMagic = 1.00;
-var weaponElement = "None";
-var numberOfAttacks = 2;
-var attackNumValue = 1.0;
-
-//Skill stats
-var skillElement = "None";
-var isSkill = false;
-var skillIsMagic = false;
-var skillDamage = 0;
-
-//Monster stats
-var monsterAttack = 0;
-var monsterDef = 3;
-var monsterMagDef = 0;
-var monsterInt = 0;
-var monsterSpd = 1;
 
 //Functions
 function calculateAttackNumValue (numberOfAttacks) {
   if (numberOfAttacks === 1) {
-    attackNumValue = 1.0;
+    const attackNumValue = 1.0;
   }
   else if (numberOfAttacks === 2) {
-    attackNumValue = 1.15;
+    const attackNumValue = 1.15;
   }
   else if (numberOfAttacks === 3) {
-    attackNumValue = 1.2;
+    const attackNumValue = 1.2;
   }
   return attackNumValue;
 }
