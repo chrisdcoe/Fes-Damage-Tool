edac3e4d979c372caef14e4bb148b340e824b69d
Christopher Coe <christopherdcoe@gmail.com>
2017-11-05 03:04:21 -0500

    Massive amounts of work on the HTML, added basic functionality to tabs, calculate Player Normal Attack actually WORKS

--- app.js
+++ app.js
@@ -1,20 +1,113 @@
 
 //Functions
-function calculateAttackNumValue (numberOfAttacks) {
-  if (numberOfAttacks === 1) {
-    const attackNumValue = 1.0;
+function calculateAttackNumValue () {
+  let numberOfAttacks = Number($("#numberOfAttacks").val());
+  if (numberOfAttacks == 2) {
+    return 1.15;
   }
-  else if (numberOfAttacks === 2) {
-    const attackNumValue = 1.15;
+  else if (numberOfAttacks == 3) {
+    return 1.20;
+  } else {
+    return 1.0;
   }
-  else if (numberOfAttacks === 3) {
-    const attackNumValue = 1.2;
-  }
-  return attackNumValue;
 }
 
-function calculatePlayerAttack (playerStr, weaponAttack, weaponElement, monsterDef, numberOfAttacks) {
+function deriveAttackPower () {
+  let playerStrength = Number($("#playerStrength").val());
+  let weaponAttack = Number($("#weaponAttack").val());
+  $("#attackPower").html(playerStrength + weaponAttack);
+}
 
-  let damageCalc = (3 * ( playerStr + weaponAttack) - monsterDef) / 4 * calculateAttackNumValue(numberOfAttacks);
-  console.log(damageCalc);
+function derivePhysicalDefense () {
+  let playerDefense = Number($("#playerDefense").val());
+  let armorDefense = Number($("#armorDefense").val());
+  $("#physDefense").html(playerDefense + armorDefense);
+}
+
+function deriveMagicalDefense () {
+  let playerIntelligence = Number($("#playerIntelligence").val());
+  let armorMagDef = Number($("#armorMagDef").val());
+  $("#magicDefense").html(playerIntelligence + armorMagDef);
+}
+
+function calculatePlayerAttack () {
+  let damageCalc = (3 * ( Number($("#attackPower").html()) ) - Number($("#monsterDefense").val())) / 4 * calculateAttackNumValue();
+  return damageCalc;
+}
+
+function refreshAttackCards() {
+  var source   = $('#attacks-template').html();
+  var template = Handlebars.compile(source);
+
+  if ($("#playerAttack").hasClass("active") ) {
+    var result = calculatePlayerAttack();
+    var resultLow = result - (result/15);
+    var resultHigh = result + (result/15);
+
+    var context = {
+      title: "Player: Normal Attack",
+      formula: "(3 * Attack - Defense) / 4 * NOH",
+      result: result,
+      resultLow: resultLow,
+      resultHigh: resultHigh
+    };
+  } else if ($("#playerDeathblow").hasClass("active")) {
+    var result = 5;
+    var resultLow = result - (result/15);
+    var resultHigh = result + (result/15);
+
+    var context = {
+      title: "Player: Deathblow",
+      formula: "(3 * Attack + 2 * Damage - Defense) / 4 * NOH",
+      result: result,
+      resultLow: resultLow,
+      resultHigh: resultHigh
+    };
+  } else if ($("#playerMagic").hasClass("active")) {
+    var result = 5;
+    var resultLow = result - (result/15);
+    var resultHigh = result + (result/15);
+
+    var context = {
+      title: "Player: Magic",
+      formula: "(2 * (Magic + Damage) * MagicMult - MDefense) / 4",
+      result: result,
+      resultLow: resultLow,
+      resultHigh: resultHigh
+    };
+  } else if ($("#playerHeal").hasClass("active")) {
+    var result = 5;
+    var resultLow = result - (result/15);
+    var resultHigh = result + (result/15);
+
+    var context = {
+      title: "Player: Heal",
+      formula: "(Effect + Magic / 4) * MagicMult",
+      result: result,
+      resultLow: result,
+      resultHigh: result
+    };
+  } else if ($("#playerItem").hasClass("active")) {
+    var result = 5;
+    var resultLow = result - (result/15);
+    var resultHigh = result + (result/15);
+
+    var context = {
+      title: "Player: Item",
+      formula: "(2 * Damage - MDefense) / 4",
+      result: result,
+      resultLow: resultLow,
+      resultHigh: resultHigh
+    };
+  }
+  // }s
+
+  var html = template(context);
+  $("#playerActionContent").html(html);
+}
+
+function playerTabChange(element) {
+  $(element).parent().siblings().children().removeClass("active");
+  $(element).addClass("active");
+  refreshAttackCards();
 }
